a8427fb18b53d1c5bf29c8d4c9f7b91659194131
raakostOnCph <36969041+raakostOnCph@users.noreply.github.com>
2022-10-02 13:39:21 +0200

    laver  emneliste

--- src/main/java/BrugerServlet.java
+++ src/main/java/BrugerServlet.java
@@ -3,6 +3,10 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 @WebServlet(name = "BrugerServlet", value = "/BrugerServlet")
 public class BrugerServlet extends HttpServlet
@@ -21,6 +25,25 @@ public class BrugerServlet extends HttpServlet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+
+        HttpSession session = request.getSession();
+
+        Set<String> emneListe = (Set<String>) session.getAttribute("emneListe");
+
+        if (emneListe == null) {
+            emneListe = new TreeSet<>();   // opretter listen vis den ikke allerede findes på sessionscopet
+
+        }
+
+        String emne = request.getParameter("emne");   // tilføjer emne til listen
+
+        emneListe.add(emne);
+        session.setAttribute("emneListe",emneListe); // hænger listen op på sessionscopet
+
+        System.out.println(emneListe.toString());
+
+        request.getRequestDispatcher("WEB-INF/brugerSide.jsp").forward(request, response);
+
 
     }
 }
--- src/main/webapp/WEB-INF/brugerSide.jsp
+++ src/main/webapp/WEB-INF/brugerSide.jsp
@@ -5,6 +5,8 @@
   Time: 09.40
   To change this template use File | Settings | File Templates.
 --%>
+
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -16,7 +18,20 @@
 du er logget ind med navnet ${requestScope.navn}
 og dit sessionid er ${sessionScope.id}
 
-<form action="BrugerServlet">
+
+<form action="BrugerServlet" method="post">
+
+    <label for="emne">Tilføj emne:</label><br>
+    <input type="text" id="emne" name="emne" value="ØlBong"><br><br>
+    <input type="submit" value="Tilføj ">
+</form>
+
+<br>
+<br>
+<br>
+
+
+<form action="BrugerServlet" method="get">
 
     <input type="submit" value="lock ud">
 
